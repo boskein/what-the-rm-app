c79f43278e73e4036e9a9992d704d9020fbf6f0d
Carlos Umaña <c.carlos.umana@gmail.com>
2020-02-28 11:28:21 -0500

    Llamada basica a la API realizada

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React from "react";
-import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Experimental fetch</h1>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {
+        results: []
+      },
+      loading: false,
+      error: false
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCharacters();
+  }
+
+  fetchCharacters = async () => {
+    this.setState({ loading: true });
+
+    const API_URL = "https://rickandmortyapi.com/api/character";
+    const response = await fetch(API_URL);
+    const characters = await response.json();
+
+    this.setState({
+      data: characters,
+      loading: false
+    });
+  };
+
+  render() {
+    if (this.state.loading) {
+      return <p>loading...</p>;
+    }
+
+    return (
+      <div className="App">
+        <h1>Experimental fetch</h1>
+        <ul>
+          {this.state.data.results.map(charachter => (
+            <li key={charachter.id}>
+              <h2>{charachter.name}</h2>
+              <p>{charachter.status}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
-
-export default App;
