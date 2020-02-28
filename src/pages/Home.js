4624cab508f60bb4d678b5f7114832a615faf5e8
Carlos Umaña <c.carlos.umana@gmail.com>
2020-02-28 13:29:28 -0500

    Nuevos estilos y contenido al homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+import "./styles/home.css";
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="Home">
+      <header className="Home__header">
+        <p className="Home__header-littleText">An app for</p>
+        <h1 className="Home__header-titlePage">Rick and Morty</h1>
+        <p className="Home__header-fans">Fans</p>
+        <p className="Home__header-descriptionApp">
+          Just a training app, here you can see all the characters of the
+          series, search by name and see more information about each character.
+          <br />
+          <span>
+            If you want to see more thecnical information about the construction
+            of this app <Link to="/about">you can!</Link>
+          </span>
+        </p>
+      </header>
     </div>
   );
 }
