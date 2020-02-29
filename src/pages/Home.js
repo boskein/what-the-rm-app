1b7c052e39c24150dd2310118bfe73b9bcf69c29
Carlos Umaña <c.carlos.umana@gmail.com>
2020-02-29 01:15:50 -0500

    Pequenhas modificaciones a los estilos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,8 +12,9 @@ export default function Home() {
         <h1 className="Home__header-titlePage">Rick and Morty</h1>
         <p className="Home__header-fans">Fans</p>
         <p className="Home__header-descriptionApp">
-          Just a training app, here you can see all the characters of the
-          series, search by name and see more information about each character.
+          Just for training, a simple app. Here you can see all the characters
+          of the series, search by name and see more information about each
+          character.
           <br />
           <span>
             If you want to see more thecnical information about the construction
