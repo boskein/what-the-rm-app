f1ef6149d156aaeed885b72443d1ac24e21af0fb
Carlos Umaña <c.carlos.umana@gmail.com>
2020-02-29 00:29:17 -0500

    Viewcharacter page anhadida

---
+++ src/pages/ViewCharacter.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default class ViewCharacter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+      loading: false,
+      error: false
+    };
+  }
+
+  componentDidMount() {
+    this.fetchSingleCharacter();
+  }
+
+  fetchSingleCharacter = async () => {
+    this.setState({ loading: true });
+
+    const API_SINGLE_CHARACTER = `https://rickandmortyapi.com/api/character/${this.props.match.params.cId}/`;
+
+    const response = await fetch(API_SINGLE_CHARACTER);
+    const singleCharacter = await response.json();
+
+    this.setState({
+      data: singleCharacter,
+      loading: false,
+      error: false
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.data.name}</h1>
+      </div>
+    );
+  }
+}
