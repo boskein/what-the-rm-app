03ae022707387cea63ff5552d80c621e0fad4df1
Carlos Umaña <c.carlos.umana@gmail.com>
2020-02-29 13:14:08 -0500

    Boton de cargar mas personajes anhadido y funcionando

--- src/Components/CharacterList.js
+++ src/Components/CharacterList.js
@@ -24,6 +24,7 @@ export default function CharacterList(props) {
           />
         ))}
       </div>
+      
     </div>
   );
 }
--- src/Components/CharacterListContainer.js
+++ src/Components/CharacterListContainer.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import "./styles/characterlist.css";
 import CharacterList from "./CharacterList";
 import Loading from "./Loading";
 
@@ -11,7 +13,8 @@ export default class CharacterListContainer extends React.Component {
         results: []
       },
       loading: false,
-      error: false
+      error: false,
+      page: 1
     };
   }
 
@@ -22,14 +25,17 @@ export default class CharacterListContainer extends React.Component {
   fetchCharacters = async () => {
     this.setState({ loading: true });
 
-    const API_URL = "https://rickandmortyapi.com/api/character";
+    const API_URL = `https://rickandmortyapi.com/api/character/?page=${this.state.page}`;
     const response = await fetch(API_URL);
     const characters = await response.json();
 
     this.setState({
-      data: characters,
+      data: {
+        results: [].concat(this.state.data.results, characters.results)
+      },
       loading: false,
-      error: false
+      error: false,
+      page: this.state.page + 1
     });
   };
 
@@ -37,6 +43,16 @@ export default class CharacterListContainer extends React.Component {
     if (this.state.loading) {
       return <Loading />;
     }
-    return <CharacterList characters={this.state.data.results} />;
+    return (
+      <React.Fragment>
+        <CharacterList characters={this.state.data.results} />
+        <button
+          className="LoadMoreContent-button"
+          onClick={() => this.fetchCharacters()}
+        >
+          Ver mas
+        </button>
+      </React.Fragment>
+    );
   }
 }
